2b4a756ff581ebe203ff7395b058a28eefb43590
Jecylove <113922554+Jecylove@users.noreply.github.com>
2022-10-26 12:05:30 +0300

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,30 @@
+ #include <iostream>
+
+  using namespace std;
+  
+   int main(){
+   	int x;
+   	int number;
+   	int total=0;
+   	
+   	while ( x <=5) 
+	   {
+     cin >> number; 
+     total = total + number;
+	   x++;
+	   }	    
+  
+  
+  
+  
+  cout << "your toatal is" << total<< endl;
+  
+  
+  
+  
+  
+  return 0;
+  }
+  	
+  	
+ 
