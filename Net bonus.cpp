07afac2f130a8c2db4f9013a6e5872c99a5c4c20
Jecylove <113922554+Jecylove@users.noreply.github.com>
2022-10-26 12:44:59 +0300

    Update Net bonus.cpp

--- Net bonus.cpp
+++ Net bonus.cpp
@@ -10,32 +10,29 @@
   int bonus;
   double total;
   
-  cout <<" enter years of work : ";
+  cout << "enter years of work : ";
   cin >> years;
-  cout<< " enter amount of salary : ";
-  cin>> salary;
+  cout << "enter amount of salary : ";
+  cin >> salary;
   
-  if ( years >=10)
+  if (years >=10)
   {
-  	bonus= 0.1* salary;
+  	bonus= 0.1 * salary;
   }
-  else if ( years>=6)	 
+  else if ( years >= 6 )	 
   {
-  	bonus = 0.08* salary;
+  	bonus = 0.08 * salary;
   }	 
   else 	 
   {
-  	bonus = 0.05* salary;
+  	bonus = 0.05 * salary;
   } 	 
 	 
    total = bonus + salary;
    
-   
-   cout << " your net bonus is " << bonus<< endl;
-   cout << " total salary is "<< total<< endl;	 
+   cout << "your net bonus is " << bonus << endl;
+   cout << "total salary is " << total << endl;	 
+	 	 
+    return 0;
 	 
-	 
-    return 0;	 
-	 
-	 	
  }
