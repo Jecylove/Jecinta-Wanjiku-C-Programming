e1636a794f017db2f8ef0185c84a10813f0e1072
Jecylove <113922554+Jecylove@users.noreply.github.com>
2022-10-26 12:35:28 +0300

    Add files via upload

---
+++ age average for a bunch of people.cpp
@@ -0,0 +1,29 @@
+ #include <iostream>
+ using namespace std;
+ 
+ int main () {
+ 
+  int age;
+  double totalAge = 0;
+  int numberOfPeopleEntered = 0;
+  
+  cout << "Enter the age of a person or -2 to quit" << endl;
+  cin >> age;
+  
+  while( age != -2) {
+  
+  	
+  
+   totalAge = totalAge + age;
+   numberOfPeopleEntered ++;
+  
+  	cout << "Enter the age of a person or -2 to quit" << endl;	
+  	cin >> age;
+  
+  }	
+  
+  cout << "The number of people entered "<< numberOfPeopleEntered << endl;
+  cout << "The average age " << totalAge/numberOfPeopleEntered << endl;
+  
+  return 0;
+ }
